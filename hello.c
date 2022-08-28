972b022ff6dc3bf73455ab280fe3cd08c556a8a6
theworldxxl <sotto5@huskers.unl.edu>
2022-08-28 03:17:13 +0000

    initial commit

---
+++ hello.c
@@ -0,0 +1,17 @@
+/**
+ * Author: Samuel O.
+ * Date: 2022/23/08
+ *
+ * A simple hellow world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+    printf("Hello World!\n");
+    printf("Samuel!\n");
+
+    return 0;
+}
